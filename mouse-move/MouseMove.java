786679bf5b50addbc5e2e30676c02955957b45cd
Julian V <ultimatej@gmx.net>
2021-12-10 15:34:20 +0100

    Add files via upload

---
+++ mouse-move/MouseMove.java
@@ -0,0 +1,80 @@
+package test;
+
+import java.awt.Robot;
+import java.awt.AWTException;
+import java.awt.HeadlessException;
+import java.awt.MouseInfo;
+import java.awt.Point;
+
+public class MouseMove
+{
+	public static final int FACTOR = 1000; // all following numbers in seconds
+	public static final int INTERVAL = 1;
+	public static final int TIMEOUT = 5;
+	public static final int DELTA = 10;
+	
+	public static void main(String[] args) throws InterruptedException
+	{		
+		Thread t = new Thread() {
+			public void run()
+			{
+				Robot r = null;
+				Point lastP, newP;
+				int unchangedCount = 0;
+				
+				lastP = MouseInfo.getPointerInfo().getLocation();
+				
+				while(true)
+				{
+					try
+					{
+						newP = MouseInfo.getPointerInfo().getLocation();
+						if(newP.equals(lastP))
+						{
+							if(unchangedCount == 0)
+							{
+								System.out.print("no motion detected");
+								r = new Robot(MouseInfo.getPointerInfo().getDevice());
+							}
+							else if(unchangedCount % TIMEOUT == 0)
+							{
+								System.out.print(":");
+								r.mouseMove((int) lastP.getX() + DELTA, (int) lastP.getY());
+								r.mouseMove((int) lastP.getX(), (int) lastP.getY());
+							}
+							else
+							{
+								System.out.print(".");								
+							}
+							unchangedCount++;
+						}
+						else
+						{
+							if(unchangedCount > 0)
+								System.out.println("moved");
+							unchangedCount = 0;
+							lastP = newP;
+							r = null;
+						}
+						Thread.sleep(INTERVAL*FACTOR);
+					}
+					catch(InterruptedException e)
+					{
+						e.printStackTrace();
+					}
+					catch(HeadlessException e)
+					{
+						e.printStackTrace();
+					}
+					catch(AWTException e)
+					{
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+		t.start();		
+		t.join();
+		System.exit(0);
+	}
+}
